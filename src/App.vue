fb32041cdf0f64efe1faa5b6823099c7c583b50c
Fabio Ribeiro de Godoy <fabio.godoy@betha.com.br>
2018-11-27 11:15:31 -0200

    Project creation

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <div class="wrapper">
+        <sidebar></sidebar>
+        <div id="content">
+            <router-view/>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sidebar from './components/Sidebar.vue'
+
+export default {
+  name: 'App',
+  components: {sidebar}
+}
+</script>
+
+<style>
+
+</style>
